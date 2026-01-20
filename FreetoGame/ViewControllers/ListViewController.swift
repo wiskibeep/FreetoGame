54b862f3a884192afa5e650bc1e294fe9b101dc5
wiski <wiski2311@gmail.com>
2026-01-20 19:18:07 +0100

    code con API de IA

--- FreetoGame/ViewControllers/ListViewController.swift
+++ FreetoGame/ViewControllers/ListViewController.swift
@@ -7,35 +7,35 @@
 
 import UIKit
 
-class ListViewController: UIViewController {
+class ListViewController: UIViewController, UITableViewDataSource {
     
+    @IBOutlet weak var tableView: UITableView!
     
-    
-    tableView: UITableView!
-    
-    
-    
-    
+    var gamelist: [Game] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        Task{
-            let gamelist = await GameProvider.getGameList()
-            let game = await GameProvider.getGameByID(id:540)
+        tableView.dataSource = self
+        
+        Task {
+            gamelist = await GameProvider.getGameList()
             
-            
-           //guard let game = game else{
-                print ( game)
-              // print ( gamelist)
-                
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
             }
-            
-            
         }
-        
-        
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return gamelist.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Game Cell", for: indexPath) as! GameViewCell
+        let game = gamelist[indexPath.row]
+        cell.configure(with: game)
+        return cell
+    }
+}
